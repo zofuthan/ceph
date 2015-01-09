e81e652e53808c549fd20bc83fa3a9e82238eeb2
Samuel Just <sjust@redhat.com>
2015-01-09 12:37:22 -0800

    fix can_discard_request

--- src/osd/PG.cc
+++ src/osd/PG.cc
@@ -5024,6 +5024,8 @@ bool PG::can_discard_request(OpRequestRef& op)
     return can_discard_replica_op<MOSDECSubOpRead, MSG_OSD_EC_READ>(op);
   case MSG_OSD_EC_READ_REPLY:
     return can_discard_replica_op<MOSDECSubOpReadReply, MSG_OSD_EC_READ_REPLY>(op);
+  case MSG_OSD_EC_READ_REPLY:
+    return can_discard_replica_op<MOSDRepScrub, MSG_OSD_REP_SCRUB>(op);
 
   case MSG_OSD_PG_SCAN:
     return can_discard_scan(op);
